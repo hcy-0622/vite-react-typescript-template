d14f079a4a9258755448721ea52678972bb2f506
Vanilla <vanilla_cy@foxmail.com>
2022-04-15 02:59:36 +0800

    配置eslint、prettier、editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import logo from "./assets/logo.svg";
-import Button from "~/components/Button";
+import logo from './assets/logo.svg'
+import Button from '~/components/Button'
 
 const App: React.FC = () => {
   return (
     <>
-      <img style={{ width: "256px" }} src={logo} />
+      <img style={{ width: '256px' }} src={logo} alt="" />
       <Button>Button</Button>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,8 @@
-const Button: React.FC<
-  React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  >
-> = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
-};
+const Button: React.FC<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>> = ({
+  children,
+  ...props
+}) => {
+  return <button {...props}>{children}</button>
+}
 
-export default Button;
+export default Button
